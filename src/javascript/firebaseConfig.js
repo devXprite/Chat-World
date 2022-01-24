6494fcc3d37105014c7b93a9aecba965a1aea2f0
7ORP3DO <davisudha806@gmail.com>
2022-01-24 17:20:03 +0530

    add all files

---
+++ src/javascript/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDA5x4V7gR8wB-Vli4ZsW2dJApHulVAYf0",
+    authDomain: "chat-app-ae863.firebaseapp.com",
+    databaseURL: "https://chat-app-ae863-default-rtdb.firebaseio.com",
+    projectId: "chat-app-ae863",
+    storageBucket: "chat-app-ae863.appspot.com",
+    messagingSenderId: "683119932272",
+    appId: "1:683119932272:web:e41451152ac2c0b2f515b5",
+    measurementId: "G-TJL5GDYXCL"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ src/javascript/main.js
@@ -0,0 +1,83 @@
+// initialize database
+const db = firebase.database();
+
+var showLastMsg = 200;
+var username;
+
+// submit form
+// listen for submit event on the form and call the postChat function
+document.getElementById("message-form").addEventListener("submit", sendMessage);
+
+// send message to db
+function sendMessage(e) {
+  e.preventDefault();
+
+  // get values to be submitted
+  var timestamp = Date.now();
+
+  var message = $('#message-input').val();
+  console.log(message);
+
+  document
+    .getElementById("message-input")
+    .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+
+  // create db collection and send in the data
+  db.ref("messages/" + timestamp).set({
+    username,
+    message,
+    timestamp
+  });
+
+  $('#message-input').val('');
+}
+
+// display the messages
+// reference the collection created earlier
+const fetchChat = db.ref("messages/");
+
+// check for new messages using the onChildAdded event listener
+fetchChat.limitToLast(showLastMsg).on("child_added", function (data) {
+
+  let messagesData = data.val();
+
+  let senderName = messagesData.username;
+  let senderMessage = messagesData.message;
+  let type = (username === senderName ? "send" : "receive");
+
+  let sendingTime = messagesData.timestamp;
+  let relativeSendingTime = moment(sendingTime, "x").fromNow();
+
+
+  const message = `
+      <div class="message ${type}" data-aos="zoom-in">
+          <p class="username">${senderName}</p>
+          <p class="msg-text">${senderMessage}</p>
+          <p class="msg-time">${relativeSendingTime}</p>
+      </div>
+  `
+
+  document.querySelector('.message-container').innerHTML += message;
+});
+
+
+//Cookies System
+
+if (Cookies.get('username')) {
+
+  username = Cookies.get('username');
+  alert(`Welcome Back ${username}`);
+
+} else {
+  setUsername()  
+}
+
+function setUsername() {
+  username = prompt("Enter Your Username");
+  if (username.length > 4) {
+  Cookies.set('username', username, { expires: 365 })    
+  } else{
+    alert("Please enter real Name.");
+    setUsername();
+  }
+}
