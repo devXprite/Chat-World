738edb525d83ed659ac544f28906b82f3e633e54
7ORP3DO <davisudha806@gmail.com>
2022-01-26 11:13:43 +0530

    add timezone

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,9 +2,14 @@
 const db = firebase.database();
 const audio = new Audio('./src/sound/Pop Up Sms Tone.mp3');
 
+// moment.tz.setDefault("Asia/Kolkata");
+
 var showLastMsg = 200;
 var username;
 
+scrollToBottom = () =>{
+  $('body').scrollTo('100%',{interrupt:true,duration:1000,queue:true})
+}
 // submit form
 // listen for submit event on the form and call the postChat function
 document.getElementById("message-form").addEventListener("submit", sendMessage);
@@ -14,20 +19,22 @@ function sendMessage(e) {
   e.preventDefault();
 
   // get values to be submitted
-  var timestamp = Date.now();
+  var localTimestamp = moment.tz("Asia/Taipei").format("x");
 
   var message = $('#message-input').val();
   console.log(message);
+  scrollToBottom();
+
 
   document
     .getElementById("message-input")
     .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 
-  // create db collection and send in the data
-  db.ref("messages/" + timestamp).set({
+  db.ref("messages/" + localTimestamp).set({
     username,
     message,
-    timestamp
+    localTimestamp,
+    serverTimestamp:firebase.database.ServerValue.TIMESTAMP
   });
 
   $('#message-input').val('');
@@ -38,32 +45,39 @@ function sendMessage(e) {
 const fetchChat = db.ref("messages/");
 
 // check for new messages using the onChildAdded event listener
-fetchChat.orderByKey().limitToLast(showLastMsg).on("child_added", function (data) {
+fetchChat.limitToLast(showLastMsg).on("child_added", function (data) {
 
   console.log('new msg recived');
   audio.play();
 
-  let messagesData = data.val();
+  try {
 
-  let senderName = messagesData.username;
-  let senderMessage = messagesData.message;
-  let type = (username.toLowerCase() === senderName.toLowerCase() ? "send" : "receive");
+    let messagesData = data.val();
 
-  let sendingTime = messagesData.timestamp;
-  // let relativeSendingTime = moment(sendingTime, "x").fromNow();
-  let relativeSendingTime = moment(sendingTime).format('MMMM Do YYYY, h:mm:ss a');
+    let senderName = messagesData.username;
+    let senderMessage = messagesData.message;
+    let type = (username.toLowerCase() === senderName.toLowerCase() ? "send" : "receive");
+
+    let sendingTimeLocal = messagesData.localTimestamp;
+    let sendingTimeServer = messagesData.serverTimestamp;
+    // let relativeSendingTime = moment(sendingTime, "x").fromNow();
+    let relativeSendingTime = moment(sendingTimeServer).format('MMMM Do YYYY, h:mm:ss a');
 
 
 
-  const message = `
-      <div class="message ${type}" data-aos="zoom-in">
+    const message = `
+      <div class="message ${type}" data-aos="zoom-in" data-aos-anchor-placement="bottom">
           <p class="username">${senderName}</p>
           <p class="msg-text">${senderMessage}</p>
           <p class="msg-time">${relativeSendingTime}</p>
       </div>
-  `
+  `;
 
-  document.querySelector('.message-container').innerHTML += message;
+    document.querySelector('.message-container').innerHTML += message;
+  } catch (error) {
+    console.log(error);
+  }
+
 });
 
 
@@ -81,18 +95,28 @@ if (Cookies.get('username')) {
 function setUsername() {
   username = prompt("Enter Your Name");
 
-  if (username == null) {
-    alert("Please to fill your name");
-    setUsername();
-  } else if (username.length > 3) {
-    Cookies.set('username', capitalizeFirstLetter(username), { expires: 365 })
-  } else {
-    alert("Please enter real Name.");
-    setUsername();
+  try {
+    if (username == null) {
+      alert("Please to fill your name");
+      setUsername();
+    } else if (username.length > 3) {
+      Cookies.set('username', capitalizeFirstLetter(username), { expires: 365 })
+    } else {
+      alert("Please enter real Name.");
+      setUsername();
+    }
+  } catch (error) {
+    console.log(error);
   }
+
+
 }
 
 function capitalizeFirstLetter(str) {
   const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
   return capitalized;
 }
+
+setTimeout(() => {
+  scrollToBottom();
+  }, 3000);
