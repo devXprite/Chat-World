2439b61e4a69f439c5b9ea6581f2343eb0fe041c
7ORP3DO <davisudha806@gmail.com>
2022-02-02 10:16:08 +0530

    update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDA5x4V7gR8wB-Vli4ZsW2dJApHulVAYf0",
-    authDomain: "chat-app-ae863.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     databaseURL: "https://chat-app-ae863-default-rtdb.firebaseio.com",
-    projectId: "chat-app-ae863",
-    storageBucket: "chat-app-ae863.appspot.com",
-    messagingSenderId: "683119932272",
-    appId: "1:683119932272:web:e41451152ac2c0b2f515b5",
-    measurementId: "G-TJL5GDYXCL"
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Initialize Firebase
